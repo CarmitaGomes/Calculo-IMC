d45d1744a5cbd26aa002ea72385c73c5f40474b9
Carmita <125024649+CarmitaGomes@users.noreply.github.com>
2026-05-29 12:43:46 +0100

    sprint 2
    Adicionada classificação mais detalhada do IMC, de acordo com as categorias da OMS

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ def calcular_IMC(peso: float, altura: float) -> float:
 
 def classificar_IMC(imc: float) -> str:
     if imc < 18.5:
-        return "Fora do peso normal"
+        return "Abaixo do peso"
     elif 18.5 <= imc < 25:
         return "Peso normal"
-    elif imc >= 25:
-        return "Fora do peso normal"
+    elif 25 <= imc < 30:
+        return "Excesso de peso"
+    elif imc >= 30:
+         return "Obesidade"
     else:
         return "Fora do peso normal"
-    
+
 def main():
     print("Calculadora de IMC")
     peso = float(input("\nDigite seu peso em kg: "))
@@ -27,3 +29,8 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# Se o IMC for menor que 18.5 -> Abaixo do peso
+#- Se o IMC for maior ou igual a 18.5 e menor que 25 -> Peso normal
+#- Se o IMC for maior ou igual a 25 e menor que 30 -> Excesso de peso
+#- Se o IMC for maior ou igual a 30 -> Obesidade
